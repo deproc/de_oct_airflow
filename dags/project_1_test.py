6e0d825b05df0fb067d6a9e7530de2909a7f2d4b
xRanSax <ransa0018@gmail.com>
2022-11-30 17:47:25 -0500

    project 1 test

---
+++ dags/project_1_test.py
@@ -0,0 +1,69 @@
+# All imports go here
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+
+# define snowflake variables
+SNOWFLAKE_CONN_ID = 'Group3'
+SNOWFLAKE_DATABASE = 'ETL_AF'
+SNOWFLAKE_SCHEMA = 'DEV_DB'
+SNOWFLAKE_ROLE = 'BF_DEVELOPER'
+SNOWFLAKE_WAREHOUSE = 'BF_ETL'
+SNOWFLAKE_STAGE = 's3_airflow_project'
+
+# SQL command
+table_name = 'PRESTAGE_USERS_GROUP3'
+create_table = (
+    f"""
+    CREATE TABLE IF NOT EXISTS {table_name}(
+   id NUMBER(38,0)
+  ,avatar VARCHAR()
+  ,email VARCHAR()
+  ,first_name VARCHAR()
+  ,last_name VARCHAR()
+  ,age NUMBER(3, 0)
+  ,Birthday DATE
+  ,country VARCHAR()
+  ,zipcode VARCHAR()
+  ,gender VARCHAR(6)
+  ,ip_address VARCHAR(15)
+  ,create_at TIMESTAMP_NTZ(9)
+    );
+    """
+)
+
+# define dag
+with DAG(
+        "Project1_Group3_test",
+        start_date=datetime(2022, 11, 27),
+        schedule_interval='59 23 * * *',
+        default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
+        tags=['beaconfire'],
+        catchup=True,
+) as dag:
+    # Create the table if does not exist
+    snowflake_table_create = SnowflakeOperator(
+        task_id='table_creation',
+        sql=create_table,
+        warehouse=SNOWFLAKE_WAREHOUSE,
+        database=SNOWFLAKE_DATABASE,
+        schema=SNOWFLAKE_SCHEMA,
+        role=SNOWFLAKE_ROLE,
+    )
+    snowflake_load_from_S3 = S3ToSnowflakeOperator(
+        task_id='table_insert',
+        s3_keys=['Users_Group3_{{ ds[0:4]+ds[5:7]+ds[8:10] }}.csv'],
+        table=table_name,
+        schema=SNOWFLAKE_SCHEMA,
+        stage=SNOWFLAKE_STAGE,
+        file_format='''(type = 'CSV', field_delimiter = ',', SKIP_HEADER = 1 \
+                NULL_IF =('NULL','null',''), empty_field_as_null = true, FIELD_OPTIONALLY_ENCLOSED_BY = '\"' \
+                ESCAPE_UNENCLOSED_FIELD = NONE RECORD_DELIMITER = '\n')''',
+    )
+    (
+        snowflake_table_create
+        >> [
+            snowflake_load_from_S3
+        ]
+    )
