bb5fb3418f25cb0f3c65bf9635beda2e3f99c657
tuyhongkhang <54401576+tuyhongkhang@users.noreply.github.com>
2022-11-30 20:34:00 -0500

    Update project1_create_table.sql

--- dags/project1_create_table.sql
+++ dags/project1_create_table.sql
@@ -8,7 +8,7 @@ create or replace table prestage_iphoneX_Group5(
     ,customer_name varchar
     ,customer_street varchar
     ,customer_city varchar
-    ,customer_state char(2)
+    ,customer_state varchar()
     ,customer_zip int
     ,order_status varchar
 );
