4223319e4cd90b675b34be4177e1c7b00c9c19e5
Qiao Xu <57956594+mortalnow@users.noreply.github.com>
2022-11-30 21:26:05 -0500

    test

--- dags/qiaoxu_test.py
+++ dags/qiaoxu_test.py
@@ -19,7 +19,7 @@ with DAG(
     "s3_data_copy_test_qiaoxu",
     start_date=datetime(2022, 11, 30),
     end_date=datetime(2022, 12, 02),
-    schedule_interval='@daily',
+    schedule_interval='00 07 * * *',
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['beaconfire'],
     catchup=True,
