02a127725d1b61e927e07c640f22fe05062985bb
Thinh Tu <tct2tf@virginia.edu>
2022-11-30 21:06:33 -0500

    added separate sql file for temp table

--- dags/prestg_product_group4.py
+++ dags/prestg_product_group4.py
@@ -19,12 +19,12 @@ SNOWFLAKE_WAREHOUSE = 'BF_ETL'
 SNOWFLAKE_STAGE = 's3_airflow_project'
 #S3_FILE_PATH = 'product_order_trans_07152022.csv'
 
-SNOWFLAKE_SAMPLE_TABLE = 'PRESTG_PRODUCT_TEMP_GROUP4'
-CREATE_TABLE_SQL_STRING = (
-    f"CREATE OR REPLACE TEMPORARY TABLE {SNOWFLAKE_SAMPLE_TABLE} (INVOICENO NUMBER(38,0), STOCKCODE VARCHAR(36), DESCRIPTION VARCHAR(100), 
-    QUANTITY NUMBER(38,0), INVOICEDATE TIMESTAMP NTZ(9), MONTH NUMBER(2,0), DAY NUMBER(2,0), UNITPRICE NUMBER(10,0), CUSTOMERID NUMBER(38,0),
-    COUNTRY VARCHAR(100), COUNTRYCODE VARCHAR(2));"
-)
+# SNOWFLAKE_SAMPLE_TABLE = 'PRESTG_PRODUCT_TEMP_GROUP4'
+# CREATE_TABLE_SQL_STRING = (
+#     f"CREATE OR REPLACE TEMPORARY TABLE {SNOWFLAKE_SAMPLE_TABLE} (INVOICENO NUMBER(38,0), \
+#         STOCKCODE VARCHAR(36), DESCRIPTION VARCHAR(100), QUANTITY NUMBER(38,0), INVOICEDATE TIMESTAMP NTZ(9), MONTH NUMBER(2,0), DAY NUMBER(2,0), UNITPRICE NUMBER(10,0), CUSTOMERID NUMBER(38,0),
+#     COUNTRY VARCHAR(100), COUNTRYCODE VARCHAR(2));"
+# )
 
 SQL_INSERT_STATEMENT = f"INSERT INTO {SNOWFLAKE_SAMPLE_TABLE} SELECT * FROM "
 
@@ -48,26 +48,32 @@ with DAG(
             ESCAPE_UNENCLOSED_FIELD = NONE, RECORD_DELIMITER = '\n')''',
     )
 
-    snowflake_op_sql_str = SnowflakeOperator(
-        task_id='snowflake_op_sql_str',
-        sql=CREATE_TABLE_SQL_STRING,
-        warehouse=SNOWFLAKE_WAREHOUSE,
-        database=SNOWFLAKE_DATABASE,
-        schema=SNOWFLAKE_SCHEMA,
-        role=SNOWFLAKE_ROLE,
-    )
+    # snowflake_op_sql_str = SnowflakeOperator(
+    #     task_id='snowflake_op_sql_str',
+    #     sql=CREATE_TABLE_SQL_STRING,
+    #     warehouse=SNOWFLAKE_WAREHOUSE,
+    #     database=SNOWFLAKE_DATABASE,
+    #     schema=SNOWFLAKE_SCHEMA,
+    #     role=SNOWFLAKE_ROLE,
+    # )
 
-    user_query_rt_rpt = SnowflakeOperator(
+    user_query_insert = SnowflakeOperator(
        task_id='product_insert',
        sql='./product_insert.sql',
        split_statements=True,
     )
 
+    user_query_temptable = SnowflakeOperator(
+       task_id='temp_table',
+       sql='./temp_table.sql',
+       split_statements=True,
+    )
+
     (
-        snowflake_op_sql_str 
+        user_query_temptable 
         >> [
         copy_into_prestg,
-        user_query_rt_rpt
+        user_query_insert
         ]
 
     )
