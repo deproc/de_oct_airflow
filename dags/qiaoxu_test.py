539461a994fa37d7088671c9dcbff36f2703f36c
Qiao Xu <57956594+mortalnow@users.noreply.github.com>
2022-12-01 03:11:54 -0500

    test

--- dags/qiaoxu_test.py
+++ dags/qiaoxu_test.py
@@ -1,4 +1,4 @@
-import os
+
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
@@ -18,18 +18,17 @@ SNOWFLAKE_STAGE = 's3_airflow_project'
 
 with DAG(
     "s3_data_copy_test_qiaoxu",
-    start_date=datetime(2022, 11, 29),
-    end_date=datetime(2022, 12, 02),
-    schedule_interval='0 5 * * *',
+    start_date=datetime(2022, 11, 30),
+    schedule_interval='@daily',
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['beaconfire'],
     catchup=True,
 ) as dag:
 
     copy_into_prestg = S3ToSnowflakeOperator(
-        task_id='prestg_QiaoXuTest_Group1',
+        task_id='QiaoXuTest_Group1',
         s3_keys=['QiaoXuRest_Group1_{{ds[0:4]+ds[5:7]+ds[8:10]}}.csv'],
-        table='prestg_QiaoXuTest_Group1',
+        table='prestg_QiaoXuTest1_Group1',
         schema=SNOWFLAKE_SCHEMA,
         stage=SNOWFLAKE_STAGE,
         file_format='''(type = 'CSV', field_delimiter = ',', SKIP_HEADER = 1 \
