3ebf575015347a8067d91461a6d2ecaa97a8320f
xRanSax <ransa0018@gmail.com>
2022-12-01 13:10:27 -0500

    table creation file for project 1

---
+++ dags/project1_table_creation.sql
@@ -0,0 +1,14 @@
+CREATE TABLE ETL_AF.DEV_DB.prestage_Users_Group3(
+   id NUMBER(38,0)
+  ,avatar VARCHAR()
+  ,email VARCHAR()
+  ,first_name VARCHAR()
+  ,last_name VARCHAR()
+  ,age NUMBER(3, 0)
+  ,Birthday DATE
+  ,country VARCHAR()
+  ,zipcode VARCHAR()
+  ,gender VARCHAR(11)
+  ,ip_address VARCHAR(15)
+  ,create_at TIMESTAMP_NTZ(9)
+);
