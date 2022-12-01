01d9da9ed853679ec2f3167c23b4a85798bc332f
Thinh Tu <tct2tf@virginia.edu>
2022-12-01 13:53:00 -0500

    changed sql update

--- dags/update_fact_table.sql
+++ dags/update_fact_table.sql
@@ -1,10 +1,10 @@
 insert into ETL_AF.DEV_DB.fact_Stock_History_Group4
 
-select * from 
-US_STOCKS_DAILY.PUBLIC.Stock_History
+select * from (
+select * from US_STOCKS_DAILY.PUBLIC.Stock_History
 EXCEPT
-ETL_AF.DEV_DB.fact_Stock_History_Group4
-    
+select * from DEV_DB.fact_Stock_History_Group4
+)
 
 
 
