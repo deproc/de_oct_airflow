c8fdb4f7f61eddc757055ad8b863bf159049890f
xfy990107 <71182779+xfy990107@users.noreply.github.com>
2022-11-30 19:11:46 -0500

    Add s3_data_copy_feiyang.py file via upload for test

---
+++ dags/s3_data_copy_feiyang.py
@@ -0,0 +1,39 @@
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+
+
+SNOWFLAKE_CONN_ID = 'feiyangxue'
+SNOWFLAKE_DATABASE = 'ETL_AF'
+SNOWFLAKE_SCHEMA = 'DEV_DB'
+
+SNOWFLAKE_ROLE = 'BF_DEVELOPER'
+SNOWFLAKE_WAREHOUSE = 'BF_ETL'
+
+SNOWFLAKE_STAGE = 's3_airflow_project'
+
+with DAG(
+    "s3_data_copy_feiyang",
+    start_date=datetime(2022, 11, 30),
+    end_date=datetime(2022, 12, 2),
+    schedule_interval='0 7 * * *',
+    default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
+    tags=['project','group1'],
+    catchup=True,
+) as dag:
+
+    copy_into_prestg = S3ToSnowflakeOperator(
+        task_id='prestg_FeiyangXue',
+        s3_keys=['QiaoXuTest_Group1_{{ds[0:4]+ds[5:7]+ds[8:10]}}.csv'],
+        table='prestg_FeiyangTest_Group1',
+        schema=SNOWFLAKE_SCHEMA,
+        stage=SNOWFLAKE_STAGE,
+        file_format='''(type = 'CSV', field_delimiter = ',', SKIP_HEADER = 1 \
+            NULL_IF =('NULL','null',''), empty_field_as_null = true, FIELD_OPTIONALLY_ENCLOSED_BY = '\"' \
+            ESCAPE_UNENCLOSED_FIELD = NONE RECORD_DELIMITER = '\n')''',
+    )
+
+    copy_into_prestg
