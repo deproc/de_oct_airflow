a0ae3ac67c72ac41b3bd465551c8ac526a7f51f3
Thinh Tu <tct2tf@virginia.edu>
2022-12-01 14:00:25 -0500

    except all

--- dags/update_fact_table.sql
+++ dags/update_fact_table.sql
@@ -2,7 +2,7 @@ insert into ETL_AF.DEV_DB.fact_Stock_History_Group4
 
 select * from (
 select * from US_STOCKS_DAILY.PUBLIC.Stock_History
-EXCEPT
+EXCEPT ALL
 select * from DEV_DB.fact_Stock_History_Group4
 )
 
