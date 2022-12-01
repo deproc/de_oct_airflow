fe1e6c043442b64fdb27adb8365c5f0217cb72d9
XLiu-02 <64922565+XLiu-02@users.noreply.github.com>
2022-12-01 11:10:48 -0600

    Update beaconfire_dev_db_test.py

--- dags/beaconfire_dev_db_test.py
+++ dags/beaconfire_dev_db_test.py
@@ -51,7 +51,7 @@ with DAG(
     snowflake_op_with_params = SnowflakeOperator(
         task_id='snowflake_op_with_params',
         sql=SQL_INSERT_STATEMENT,
-        parameters={"id": 11},
+        parameters={"id": 5},
         warehouse=SNOWFLAKE_WAREHOUSE,
         database=SNOWFLAKE_DATABASE,
         schema=SNOWFLAKE_SCHEMA,
