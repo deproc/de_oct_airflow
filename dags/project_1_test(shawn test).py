0d4ec6b38ea597d3954493cde9aa985158b27e89
dzwu <dezhang.wu@gmail.com>
2022-12-01 11:04:03 -0500

    login test6

--- dags/project_1_test(shawn test).py
+++ dags/project_1_test(shawn test).py
@@ -42,7 +42,7 @@ create_table = (
 # define dag
 with DAG(
         "Project1_Group3_Shawn_test",
-        start_date=datetime(2022, 11, 29),
+        start_date=datetime(2022, 11, 29, tz='US/Eastern'),
         schedule_interval='0 7 * * *',
         default_args= {'snowflake_conn_id': SNOWFLAKE_CONN_ID},
         tags=['beaconfire'],
