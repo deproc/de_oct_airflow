5d7b4f2b985e9ff2ea6a6adce69b724665cfc504
KHANG TUY <tuyhongkhang@gmail.com>
2022-12-02 00:28:59 -0500

    khang

--- dags/project2_snowflake_to_snowflake.py
+++ dags/project2_snowflake_to_snowflake.py
@@ -16,7 +16,7 @@ SNOWFLAKE_WAREHOUSE = 'BF_ETL'
 SNOWFLAKE_STAGE = 's3_airflow_project' 
 # S3_FILE_PATH = 'iphoneX_Group5_20221130.csv'
 
-UPDATE_DIM_TABLE_SQL = '''create or replace DIM_COMPANY_PROFILE_GROUP5 
+UPDATE_DIM_TABLE_SQL = '''create or replace dim_company_profile_group5 
                         clone US_STOCKS_DAILY.PUBLIC.COMPANY_PROFILE;'''
 
 with DAG(
