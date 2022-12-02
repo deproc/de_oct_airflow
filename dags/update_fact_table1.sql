76e0982e40f555263b40ba1074fdc2ebf23aa5ce
Thinh Tu <tct2tf@virginia.edu>
2022-12-02 12:02:36 -0500

    insert method for stock history

--- dags/update_fact_table1.sql
+++ dags/update_fact_table1.sql
@@ -1,21 +1,20 @@
 
 -- insert into ETL_AF.DEV_DB.fact_Stock_History_Group4
 
--- select * from (
--- select * from US_STOCKS_DAILY.PUBLIC.Stock_History
--- EXCEPT
--- select * from ETL_AF.DEV_DB.fact_Stock_History_Group4
--- )
 
  --stock history
-merge into ETL_AF.DEV_DB.fact_Stock_History_Group4 as a using US_STOCKS_DAILY.PUBLIC.Stock_History as b
-    on (a.symbol = b.symbol and a.date = b.date and a.open = b.open and a.high = b.high and a.low = b.low and a.close = b.close 
-    and a.volume = b.volume and a.adjclose = b.adjclose)
+--merge into ETL_AF.DEV_DB.fact_Stock_History_Group4 as a using US_STOCKS_DAILY.PUBLIC.Stock_History as b
+   -- on (a.symbol = b.symbol and a.date = b.date and a.open = b.open and a.high = b.high and a.low = b.low and a.close = b.close 
+  --  and a.volume = b.volume and a.adjclose = b.adjclose)
     -- when matched then 
     --    update set a.symbol = b.symbol, a.date = b.date, a.open = b.open, a.high = b.high, a.low = b.low, a.close = b.close,
     -- a.volume = b.volume, a.adjclose = b.adjclose
-    when not matched then 
-        insert (symbol, date, open, high, low, close, volume, adjclose) values (b.symbol, b.date, b.open, b.high, b.low, b.close, b.volume, b.adjclose);
+ --   when not matched then 
+ --       insert (symbol, date, open, high, low, close, volume, adjclose) values (b.symbol, b.date, b.open, b.high, b.low, b.close, b.volume, b.adjclose);
+insert into "ETL_AF"."DEV_DB"."FACT_STOCK_HISTORY_GROUP4" 
+select a1.id, a1.symbol, a1.date, a1.open, a1.high, a1.low, a1.close, a1.volume, a1.adjclose
+from  US_STOCKS_DAILY.PUBLIC.Stock_History a1
+where current_date()=date
 
 
 merge into ETL_AF.DEV_DB.DIM_COMPANY_PROFILE_GROUP4 as a
@@ -48,6 +47,3 @@ create or replace table  "ETL_AF"."DEV_DB"."DIM_SYMBOLS_GROUP4"  as
 select  id, symbol, name, exchange, row_number() over(order by id) as symbol_id 
 from "US_STOCKS_DAILY"."PUBLIC"."SYMBOLS"
 
---insert method
---insert into ETL_AF.DEV_DB.fact_Stock_History_Group4 
---select * from US_STOCKS_DAILY.PUBLIC.Stock_History
