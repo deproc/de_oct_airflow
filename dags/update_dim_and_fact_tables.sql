fd653c432f197cc4ccb6481a944dc4214bb6486a
ZackZhao556 <zackzhao2020@163.com>
2022-12-02 20:00:16 -0600

    modified version

--- dags/update_dim_and_fact_tables.sql
+++ dags/update_dim_and_fact_tables.sql
@@ -1,29 +1,40 @@
--- ONLY INSERT THE DATA THAT'S NOT IN FACT TABLE
-INSERT INTO "ETL_AF"."DEV_DB"."FACT_STOCK_GROUP1"
--- FILTER DATA BY ID AND DATE
-WITH NEW_ENTRY AS
-(SELECT 
-    * 
- FROM "US_STOCKS_DAILY"."PUBLIC"."STOCK_HISTORY"
- WHERE (ID, DATE) NOT IN (SELECT 
-                            ID
-                            ,DATE
-                          FROM "ETL_AF"."DEV_DB"."FACT_STOCK_GROUP1"))
--- FIND CORRESPONDING ID BASED ON SYMBOLS IN THE SYMBOLS TABLE
-SELECT 
-    S.ID AS ID
-    ,DATE
-    ,OPEN
-    ,HIGH
-    ,LOW
-    ,CLOSE
-    ,VOLUME
-    ,ADJCLOSE
- FROM NEW_ENTRY T
- JOIN "US_STOCKS_DAILY"."PUBLIC"."SYMBOLS" S 
- ON T.SYMBOL = S.SYMBOL;
+CREATE OR REPLACE TEMPORARY TABLE "ETL_AF"."DEV_DB"."TEMP" AS
+SELECT
+    sym.id AS symbol_id
+    ,c.id AS company_id
+    ,s.date
+    ,s.open
+    ,s.high
+    ,s.low
+    ,s.close
+    ,s.volume
+    ,s.adjclose
+FROM "US_STOCKS_DAILY"."PUBLIC"."STOCK_HISTORY" s
+LEFT JOIN "US_STOCKS_DAILY"."PUBLIC"."SYMBOLS" sym
+ON s.symbol = sym.symbol
+LEFT JOIN "US_STOCKS_DAILY"."PUBLIC"."COMPANY_PROFILE" c
+ON c.symbol=s.symbol
+EXCEPT
+SELECT
+    *
+FROM "ETL_AF"."DEV_DB"."FACT_STOCK_GROUP1";
 
--- Insert data into the dimension table in the same way
-INSERT INTO "ETL_AF"."DEV_DB"."DIM_STOCK_GROUP1"
-SELECT * FROM "US_STOCKS_DAILY"."PUBLIC"."COMPANY_PROFILE"
-WHERE ID NOT IN (SELECT ID FROM "ETL_AF"."DEV_DB"."DIM_STOCK_GROUP1");
+-- Use merge to insert new data or update the old data
+MERGE "ETL_AF"."DEV_DB"."FACT_STOCK_GROUP1" t
+USING "ETL_AF"."DEV_DB"."TEMP" s
+ON t.symbol_id = s.symbol_id AND t.company_id = s.company_id AND t.date = s.date
+WHEN MATCHED THEN UPDATE SET t.open = s.open, t.high = s.high, t.low = s.low, t.close = s.close, t.volume = s.volume, t.adjclose = s.adjclose
+WHEN NOT MATCHED THEN INSERT (symbol_id, company_id, date, open, high, low, close, volume, adjclose)
+    VALUES (s.symbol_id, s.company_id, s.date, s.open, s.high, s.low, s.close, s.volume, s.adjclose);
+
+-- Insert or update data into the dimension table
+CREATE OR REPLACE TABLE "ETL_AF"."DEV_DB"."DIM_STOCK_GROUP1" as
+SELECT * FROM "US_STOCKS_DAILY"."PUBLIC"."COMPANY_PROFILE";
+
+CREATE OR REPLACE TABLE "ETL_AF"."DEV_DB"."DIM2_STOCK_GROUP1" as
+SELECT * FROM "US_STOCKS_DAILY"."PUBLIC"."SYMBOLS";
+
+
+
+
+
