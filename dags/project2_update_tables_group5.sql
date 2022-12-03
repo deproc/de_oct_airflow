ecb3d1bff80d218430e4d626bef523da4fa442fe
XLiu-02 <64922565+XLiu-02@users.noreply.github.com>
2022-12-03 00:55:05 -0600

    Update the fact table option 2 complete version

--- dags/project2_update_tables_group5.sql
+++ dags/project2_update_tables_group5.sql
@@ -59,23 +59,38 @@ on a.symbol = b.symbol
 -- If we only use merge, we will need to update all the existing rows.
 -- To minimize the number of rows to update, we first find all the differences using except
 -- Assume that nothing has been deleted in the source file
--- create or replace temporary table "ETL_AF"."DEV_DB".table_changes_group5
--- select * from "US_STOCKS_DAILY"."PUBLIC"."COMPANY_PROFILE"
+
+-- create or replace temporary table "ETL_AF"."DEV_DB".Updated_FACT_STOCK_HISTORY_GROUP5 like
+-- "ETL_AF"."DEV_DB"."FACT_STOCK_HISTORY_GROUP5";
+
+-- insert into "ETL_AF"."DEV_DB".Updated_FACT_STOCK_HISTORY_GROUP5
+-- select
+--     c.id
+--     ,s.date
+--     ,s.open
+--     ,s.high
+--     ,s.low
+--     ,s.close
+--     ,s.volume
+--     ,s.adjclose
+-- from "US_STOCKS_DAILY"."PUBLIC"."STOCK_HISTORY" s
+--     left join "US_STOCKS_DAILY"."PUBLIC"."COMPANY_PROFILE" c
+--         on c.symbol=s.symbol;
+
+-- create or replace temporary table "ETL_AF"."DEV_DB".table_changes_group5 as
+-- select * from "ETL_AF"."DEV_DB".Updated_FACT_STOCK_HISTORY_GROUP5
 -- except 
--- select $2,$3,$2,$3,$2,$3,$2,$3,$2,$3,$2,$3
--- from "ETL_AF"."DEV_DB"."FACT_STOCK_HISTORY_GROUP5"
+-- select * from "ETL_AF"."DEV_DB"."FACT_STOCK_HISTORY_GROUP5";
 
 -- --then, we merge the temp table into our target using merge
--- merge into "ETL_AF"."DEV_DB"."DIM_COMPANY_PROFILE_GROUP5" a
--- using "ETL_AF"."DEV_DB".dim_table_updated_group5 b
+
+-- merge into "ETL_AF"."DEV_DB"."FACT_STOCK_HISTORY_GROUP5" a
+-- using "ETL_AF"."DEV_DB".table_changes_group5 b
 -- on  a.$1=b.$1
 -- when matched then update 
 -- set a.$2=b.$2, a.$3= b.$3, a.$4=b.$4, a.$5= b.$5,
---     a.$6=b.$6, a.$7= b.$7, a.$8=b.$8, a.$9= b.$9,
---     a.$10=b.$10, a.$11= b.$11, a.$12=b.$12, a.$13= b.$13,
---     a.$14=b.$14, a.$15= b.$15, a.$16=b.$16, a.$17= b.$17, a.$18= b.$18
+--     a.$6=b.$6, a.$7= b.$7, a.$8=b.$8
 -- when not matched then insert
---     values (b.$1,b.$2,b.$3,b.$4,b.$5,b.$6,b.$7,b.$8,b.$9,b.$10,
---             b.$11,b.$12,b.$13,b.$14,b.$15,b.$16,b.$17,b.$18)
+--     values (b.$1,b.$2,b.$3,b.$4,b.$5,b.$6,b.$7,b.$8)
 
 
