186d421a41d3971f761659c1c5efeafc975f3a9c
Graham-Yin <88560277+Graham-Yin@users.noreply.github.com>
2022-12-02 23:06:58 -0600

    Create Airflow Project2.py

---
+++ sql_scripts_for_DAGs/Airflow Project2.py
@@ -0,0 +1,28 @@
+import os
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+
+# declaring varibles:
+
+SNOWFLAKE_CONN_ID = 'snowflake_conn'
+DAG_ID = "Airflow_project_2"
+
+# [START howto_operator_snowflake]
+with DAG(
+        DAG_ID,
+        start_date=datetime(2022, 12 , 1),
+        schedule_interval='00 14 * * *',
+        default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
+        tags=['beaconfire'],
+        catchup=True,
+) as dag:
+    # [START snowflake_dag]
+    snowflake_update_target_tables = SnowflakeOperator(
+        task_id='update_target_tables',
+        sql='Airflow_Project_2_ETL.sql',
+        split_statements = True,
+    )
+
+
+    snowflake_update_target_tables
